683c96bdaf2025773fc0defca664c155c6beb6d5
Logan W <wattloga@isu.edu>
2018-09-26 19:37:09 -0600

    Add Value script.

---
+++ 1610/Assets/Scripts/AddValue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class AddValue : ScriptableObject 
+{
+    public FloatData ValueObj;
+
+	public void AddValueToObj (FloatData data)
+	 {
+	    ValueObj.Value += data.Value;	
+	}
+	
+	 
+		
+	
+}
