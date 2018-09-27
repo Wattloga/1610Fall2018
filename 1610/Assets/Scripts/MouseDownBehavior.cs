b71066fafeefcb005b2e6a122e5efc9523dcc964
Logan W <wattloga@isu.edu>
2018-09-26 19:36:56 -0600

    Mouse Down Script

---
+++ 1610/Assets/Scripts/MouseDownBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDownBehavior : MonoBehaviour
+{
+ public UnityEvent Event;
+ private void OnMouseDown()
+ {
+   Event.Invoke();
+ }
+   
+}
+		
+
+	
