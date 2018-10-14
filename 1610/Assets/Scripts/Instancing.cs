e4d8088e58f4309f8db4037706b477e3c787fe06
Logan W <wattloga@isu.edu>
2018-10-14 09:57:00 -0600

    Instancing Script.
    Instancing C# script created for the ammo.

---
+++ 1610/Assets/Scripts/Instancing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking.PlayerConnection;
+
+public class Instancing : MonoBehaviour
+{
+
+	public GameObject Instance;
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.X))
+		{
+			Instantiate(Instance, transform.position, transform.rotation);
+			
+		}
+	}
+	
+}
