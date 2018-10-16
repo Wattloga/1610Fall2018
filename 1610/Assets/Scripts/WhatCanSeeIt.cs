cd160d3a4292bd7f39cbddbde790cc066da9ea0f
Logan W <wattloga@isu.edu>
2018-10-15 21:53:22 -0600

    WhatCanSeeIt Script

---
+++ 1610/Assets/Scripts/WhatCanSeeIt.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhatCanSeeIt: ScriptableObject
+{
+
+	private float health;
+	protected float PowerLevel;
+	public float EneryLevel;
+
+	private void AddHealth()
+	{
+		health += 100;
+	}
+
+	protected void AddPower()
+	{
+		AddHealth();
+	}
+
+	public void AddEnergy()
+	{
+		
+	}
+	
+}
+
+public class ICanSeeIt: MonoBehaviour
+{
+	private WhatCanSeeIt newObject;
+
+	private void Start()
+	{
+		newObject.EneryLevel = 100;
+		newObject.AddEnergy();
+	}
+}
+
+public class Child : WhatCanSeeIt
+{
+	void CallThings()
+	{
+		PowerLevel = 200;
+		EneryLevel = 300;
+		AddPower();
+		AddEnergy();
+	}
+}
