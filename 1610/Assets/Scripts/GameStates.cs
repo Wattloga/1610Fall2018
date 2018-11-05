5ec4e5442c5585bd8176fd61faa076c0b0bca817
Logan W <wattloga@isu.edu>
2018-11-05 16:12:29 -0700

    Game States Script
    Created a new C# script for game states with Starting, Loading, Playing and Ending.

---
+++ 1610/Assets/Scripts/GameStates.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStates  {
+
+   public enum States
+   {
+	  Starting,
+	  Loading,
+	  Playing,
+	  Ending
+   }
+
+	public States State;
+}
