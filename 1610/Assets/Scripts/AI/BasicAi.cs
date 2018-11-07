214d0a6d167d3328f17a1bdb953022fda588b658
Logan W <wattloga@isu.edu>
2018-11-06 19:03:13 -0700

    Basic AI script
    Created a basic AI C# script for a capsule.

---
+++ 1610/Assets/Scripts/AI/BasicAi.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicAi : MonoBehaviour
+{
+
+	private NavMeshAgent agent;
+	public Transform Destination;
+	// Use this for initialization
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		agent.destination = Destination.position;
+	}
+}
