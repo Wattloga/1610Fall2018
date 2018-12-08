c9370a5a9c535b76d906eb9e17c4d10fe862feb8
Logan W <wattloga@isu.edu>
2018-12-08 00:47:48 -0700

    Player Movement Script
    Created a player movement script for my cube.

---
+++ 1610/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	public Rigidbody rb;
+	public float movingForce = 1000f;
+	public float sidewaysForce = 1000f;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		rb.AddForce(0, 0, movingForce * Time.deltaTime);
+
+
+		if (Input.GetKey("f") )
+		{
+			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+		}
+		
+		
+		if (Input.GetKey("b") )
+		{
+			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+		}
+	}
+	
+}
