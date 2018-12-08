f98d19e569d04f1d8838e8b335d4ba5fec1bdd37
Logan W <wattloga@isu.edu>
2018-12-08 00:48:21 -0700

    Player Colliding Script
    Created a colliding script for the player to interact with hitting walls.

---
+++ 1610/Assets/Scripts/PlayerColliding.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColliding : MonoBehaviour
+{
+
+    public CharacterMover Mover;
+    
+    //public Vector3 velocity;
+
+    private void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(gameObject.name + "has collided with" + col.gameObject.name);
+        Mover.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(gameObject.name + "was triggered by" + other.gameObject.name);
+    }
+}
+
